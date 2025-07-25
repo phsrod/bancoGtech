94487c2e22c0dc040ebfd5f9d4b51a3acfff81f5
phsrod <pedrohsr2017@outlook.com>
2025-07-25 16:06:37 -0300

    removendo db_config.py com senha exposta

--- bancogtech/bd/db_config.py
+++
@@ -1,7 +0,0 @@
-db_config = {
-    'host': 'localhost',       # porque seu MySQL está na sua máquina via Docker
-    'user': 'root',            # usuário padrão, ou o que você usa
-    'password': 'root',        # a senha que você configurou no container MySQL
-    'database': 'bd',          # o banco que você criou no Workbench
-    'port': 3306,              # porta padrão do MySQL
-}
